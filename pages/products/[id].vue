5342f19a26636bf9cc7ba49fd2df6a3c9de6a5c9
= <=>
2022-11-18 15:14:54 -0500

    agregue un archivo en la carpeta products para que me muestre la informacion de un producto con el id que se le pase por la url

---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <p>Product id is {{id}}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nihil, repellat quasi quaerat explicabo ipsam temporibus nulla officia aliquid voluptatibus omnis unde enim maiores sint blanditiis tempora quae soluta consequatur.</p>
+</template>
+
+<script setup>
+const {id} = useRoute.params
+</script>
+
+<style scoped>
+
+</style>
