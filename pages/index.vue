d97caa272d1f9018bae9235d92d298e65e1e0751
= <=>
2022-11-18 14:54:34 -0500

    instalacion del proyecto, eliminacion del componente raiz y creacion de la carpeta pages como punto de entrada de la aplicacion, tambien se creó la carpeta products para trabajar los productos

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel in repellat quia neque aperiam magnam tempora sit corporis reiciendis, non eius incidunt nesciunt sunt facilis, quam enim deserunt. Dicta, suscipit.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel in repellat quia neque aperiam magnam tempora sit corporis reiciendis, non eius incidunt nesciunt sunt facilis, quam enim deserunt. Dicta, suscipit.</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2{
+    margin-bottom: 16px;
+    font-size: 36px;
+}
+
+p{
+    margin: 20px 0;
+}
+</style>
