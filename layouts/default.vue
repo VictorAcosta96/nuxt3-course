1ba84d8f85b530b96dd4f88136259996231e1907
= <=>
2022-11-19 23:39:57 -0500

    cree la carpeta layouts para definir las partes reusables de la aplicacion y cree un componente llamado default que contendrá el menu de navegacion de la pagina, tambien cree un componente para la vista general de productos y que será compartida con un producto individual

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <header>
+        <NuxtLink to="/products"> NuxtCourse </NuxtLink>
+        <nav>
+            <ul>
+                <li><NuxtLink to="/"> Home </NuxtLink></li>
+                <li><NuxtLink to="/about"> About </NuxtLink></li>
+                <li><NuxtLink to="/products"> products </NuxtLink></li>
+            </ul>
+        </nav>
+    </header>
+
+   <!-- usaremos el componente slot para mostrar el contenido de la pagina en la que llamemos este componente(default) -->
+   <div>
+       <slot />
+   </div>
+</template>
+
+<style scoped>
+.router-link-exact-active{
+    color: #12b488;
+}
+</style>
---
+++ layouts/products.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <header>
+        <nav>
+            <NuxtLink to="/products"> NuxtCourse </NuxtLink>
+        </nav>
+    </header>
+
+   <!-- usaremos el componente slot para mostrar el contenido de la pagina en la que llamemos este componente(default) -->
+   <div>
+       <slot />
+   </div>
+
+   <footer>
+    <ul>
+        <li><NuxtLink to="/"> Home </NuxtLink></li>
+        <li><NuxtLink to="/about"> About </NuxtLink></li>
+        <li><NuxtLink to="/products"> products </NuxtLink></li>
+    </ul>
+   </footer>
+</template>
+
+<style scoped>
+.router-link-exact-active{
+    color: #12b488;
+}
+</style>
